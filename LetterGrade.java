c6d4bc7e8e8c5173d33f2290f0880dec6121168b
mmvssumanth <146044369+mmvssumanth@users.noreply.github.com>
2023-10-02 11:30:48 -0500

    Update LetterGrade.java

--- LetterGrade.java
+++ LetterGrade.java
@@ -1,45 +1,46 @@
-// ocu Software Engineer
-// Team  : Malleswara Venkata Satyasai Sumanth Maradugu, naga sai garnepudi
-
+// Author: Malleswara Venkata Satyasai Sumanth Maradugu
 import java.util.Scanner;
 
 public class LetterGrade {
     public static void main(String[] args) {
-        char LetterGrade;
-		Scanner scan = new Scanner(System.in);
-        System.out.println("Malleswara Venkata Satyasai Sumanth Maradugu and naga sai garnepudi");
-        System.out.println("Please enter your grade: ");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Malleswara Venkata Satyasai Sumanth Maradugu");
+        System.out.println("Please enter your grade  ");
+
         while (true) {
             String input = scan.next();
 
             if (input.equals("q")) {
-                break; // Exit the loop if 'q' is entered
+                break; 
             }
 
             try {
                 int grade = Integer.parseInt(input);
-                char letterGrade = getLetterGrade(grade);
+                String letterGrade = getLetterGrade(grade);
                 System.out.println("The letterGrade for the grade " + grade + " is " + letterGrade);
             } catch (NumberFormatException e) {
-               
+                System.out.println("Invalid input. Please enter a numeric grade or 'q' to quit.");
             }
         }
+        scan.close();
     }
 
-    public static char getLetterGrade(int grade) {
+    public static String getLetterGrade(int grade) {
         if (grade >= 90) {
-            return 'A+';
+            return "A+";
         } else if (grade >= 80) {
-            return 'B+';
+            return "B+";
         } else if (grade >= 70) {
-            return 'C+';
-        } else if (grade >= 60) {
-            return 'D+';
+            return "C+";
+        } else if (grade >= 45) {
+            return "D+";
         } else {
-            return 'F';
+            return "F";
         }
     }
 }
+
 
 
 
